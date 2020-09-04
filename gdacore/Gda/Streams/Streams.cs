d6d881bf7032bd076c01d2ae29dce0dde25c67be
Brimerland <github@bjoern.gdanietz.com>
2020-09-04 15:40:27 +0200

    - new Network/Stream design

--- gdacore/Gda/Streams/Streams.cs
+++ gdacore/Gda/Streams/Streams.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+using System.Net.Sockets;
+using System.Net;
 
 namespace Gda.Streams
 {
@@ -10,5 +15,192 @@ namespace Gda.Streams
 
     class Connection
     {
+    }
+
+    interface IConnection<T>
+    {
+        Task SendAsync(ReadOnlyMemory<T> toSend);
+
+        Task WhenShutdownAsync();
+    }
+
+    interface IConnectable<T>
+    {
+        void ConnectDown(IConnection<T> newDown);
+    }
+
+    class BufferBlockConnection<T> : IConnection<T>, IConnectable<T> 
+    {
+        IConnection<T> Down;
+        BufferBlock<ReadOnlyMemory<T>> OutBufferBlock = new BufferBlock<ReadOnlyMemory<T>>();
+
+        Task SendLoopTask;
+        TaskCompletionSource<bool> TCS_Shutdown = new System.Threading.Tasks.TaskCompletionSource<bool>();
+
+        public Task WhenShutdownAsync() => TCS_Shutdown.Task;
+
+        public void ConnectDown(IConnection<T> newDown)
+        {
+            Down = newDown;
+        }
+
+        public Task SendAsync(ReadOnlyMemory<T> toSend)
+        {
+            if (SendLoopTask == null)
+            {
+                lock(this)
+                {
+                    if (SendLoopTask == null)
+                        SendLoopTask = Task.Run(() => SendLoopAsync(TCS_Shutdown.Task));
+                }
+            }
+
+            return OutBufferBlock.SendAsync(toSend);
+        }
+
+        async Task SendLoopAsync(Task shutdownTask)
+        {
+            var availableTask = OutBufferBlock.OutputAvailableAsync();
+            var buffer = new T[65535];
+            var currentItems = new List<ReadOnlyMemory<T>>();
+            while (shutdownTask != await Task.WhenAny(shutdownTask, availableTask) && availableTask.Result)
+            {
+                try 
+                {
+                    currentItems.Clear();
+                    
+                    var totalLength = 0; 
+                    while (totalLength < buffer.Length && OutBufferBlock.TryReceive(out var item))
+                    {
+                        totalLength += item.Length;
+                        currentItems.Add(item);
+                    }
+
+                    var down = Down;
+                    if (down != null)
+                    {
+                        if (totalLength > buffer.Length)
+                            buffer = new T[totalLength];
+
+                        ReadOnlyMemory<T> _concat()
+                        {
+                            var bufferSpan = new Span<T>(buffer);
+                            int pos = 0;
+                            foreach(var item in currentItems)
+                            {
+                                item.Span.CopyTo(bufferSpan.Slice(pos));
+                                pos += item.Length;
+                            }
+
+                            return new ReadOnlyMemory<T>(buffer, 0, pos);
+                        }
+
+                        await Down.SendAsync(_concat());
+                    }
+                }
+                catch
+                {}
+
+                availableTask = OutBufferBlock.OutputAvailableAsync();
+            }
+        }
+    }
+
+    class SocketConnectionOut : IConnection<byte>
+    {
+        public Socket Socket;
+        
+        public async Task SendAsync(ReadOnlyMemory<byte> toSend)
+        {
+            await Socket.SendAsync(toSend, SocketFlags.None);
+        }
+
+        public Task WhenShutdownAsync() => null;
+    }
+
+    class SocketConnectionIn : IConnectable<byte>
+    {
+        public Socket Socket;
+
+        IConnection<byte> Down;
+
+        public void ConnectDown(IConnection<byte> newDown)
+        {
+            Down = newDown;
+        }
+
+        public Task StartReceiveAsync()
+        {
+            return Task.Run(async () =>
+            {
+                var buffer = new byte[65535];
+                var bufferMem = new Memory<byte>(buffer);
+                var readBytes = 0;
+                do 
+                {
+                    readBytes = await Socket.ReceiveAsync(bufferMem, SocketFlags.None);
+                    await Down.SendAsync(bufferMem.Slice(0,readBytes));
+                } while (readBytes > 0);
+            });
+        }
+    }
+
+    class SocketSource : IConnectable<Socket>
+    {
+        IConnection<Socket> Down;
+        public void ConnectDown(IConnection<Socket> newDown)
+        {
+            Down = newDown;
+        }
+
+        public Task StartReceiveAsync()
+        {
+            return Task.Run(async () => 
+            {
+                var lSocket = new Socket(AddressFamily.InterNetwork,
+                                     SocketType.Stream,
+                                     ProtocolType.Tcp);
+
+                lSocket.Bind(new IPEndPoint(IPAddress.Any, 1704));
+
+                // start listening
+                lSocket.Listen(10);
+                while (true)
+                {
+                    var asocket = await lSocket.AcceptAsync();
+                    var down = Down;
+                    if (down != null)
+                        _ = down.SendAsync(new ReadOnlyMemory<Socket>(new Socket[] { asocket }));
+                }
+            });
+        }
+    }
+
+    class SocketConsumer : IConnection<Socket>, IConnectable<(SocketConnectionIn, SocketConnectionOut)>
+    {
+        IConnection<(SocketConnectionIn, SocketConnectionOut)> Down;
+
+        public void ConnectDown(IConnection<(SocketConnectionIn, SocketConnectionOut)> newDown)
+        {
+            Down = newDown;
+        }
+
+        public Task SendAsync(ReadOnlyMemory<Socket> toSend)
+        {
+            foreach(var socket in toSend.ToArray())
+            {
+                Console.WriteLine($"new Socket from {socket.RemoteEndPoint.ToString()}");
+                var down = Down;
+                if (down != null)
+                    _ = down.SendAsync(new ReadOnlyMemory<(SocketConnectionIn, SocketConnectionOut)>( new (SocketConnectionIn, SocketConnectionOut)[] 
+                        { (new SocketConnectionIn() {Socket = socket}, new SocketConnectionOut() {Socket = socket})  }));
+                else
+                    socket.Close();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task WhenShutdownAsync() => null;
     }
 }
--- gdacore/Program.cs
+++ gdacore/Program.cs
@@ -1,16 +1,93 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace gdacore
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World! " + Gda.Dingens.SomeString);
-            Gda.MidiIn.Create();
+            //Gda.MidiIn.Create();
 
-            var conn = new Gda.Streams.Connection();
+            Task shutdownTask = null;
 
+            try
+            {
+                var conn = new Gda.Streams.Connection();
+                var bbCon = new Gda.Streams.BufferBlockConnection<byte>();
+                bbCon.ConnectDown(new DownTerminal());
+                await bbCon.SendAsync(new ReadOnlyMemory<byte>(new byte[255]));
+                shutdownTask = bbCon.WhenShutdownAsync();
+                var downSource = new DownSource();
+                downSource.ConnectDown(bbCon);
+                _= downSource.RunAsync();
+
+                var ssource = new Gda.Streams.SocketSource();
+                var sconsumer = new Gda.Streams.SocketConsumer();
+                ssource.ConnectDown(sconsumer);
+                sconsumer.ConnectDown(new SocketTerminal());
+                _ = ssource.StartReceiveAsync();
+            }
+            catch (Exception e)
+            {}
+            finally
+            {}
+
+
+            await (shutdownTask??Task.CompletedTask);
         }
+    }
+
+    class DownSource : Gda.Streams.IConnectable<byte>
+    {
+        Gda.Streams.IConnection<byte> Down;
+        public void ConnectDown(Gda.Streams.IConnection<byte> newDown)
+        {
+            Down = newDown;
+        }
+
+        public Task RunAsync()
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var line = await Console.In.ReadLineAsync();
+                    var down = Down;
+                    if (down != null)
+                        await down.SendAsync(new ReadOnlyMemory<byte>(System.Text.Encoding.UTF8.GetBytes(line)));
+                }
+            });
+        }
+
+    }
+
+    class DownTerminal : Gda.Streams.IConnection<byte>
+    {
+        public Task SendAsync(ReadOnlyMemory<byte> toSend)
+        {
+            Console.WriteLine($"Sending {toSend.Length} bytes");
+            Console.WriteLine($"'{System.Text.Encoding.UTF8.GetString(toSend.Span)}'");
+            return Task.CompletedTask;
+        }
+
+        public Task WhenShutdownAsync() => null;
+    }
+
+    class SocketTerminal : Gda.Streams.IConnection<(Gda.Streams.SocketConnectionIn, Gda.Streams.SocketConnectionOut)>
+    {
+
+        public async Task SendAsync(ReadOnlyMemory<(Gda.Streams.SocketConnectionIn, Gda.Streams.SocketConnectionOut)> toSend)
+        {
+            foreach(var socketPair in toSend.ToArray())
+            {
+                var inCon = socketPair.Item1;
+                inCon.ConnectDown(new DownTerminal());
+                _ = inCon.StartReceiveAsync();
+            }
+        }
+
+        public Task WhenShutdownAsync() => null;
     }
 }
